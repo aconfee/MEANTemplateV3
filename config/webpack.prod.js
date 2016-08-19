9d8437a27503352fc68e5384d14afb974c96efce
System Administrator <root@Adams-MacBook-Air.local>
2016-08-18 23:36:54 -0500

    prod build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: '/',
+    publicPath: 'https://young-savannah-99031.herokuapp.com/',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
